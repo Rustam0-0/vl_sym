6ee933fbea8bf9f0852fc0fba221236021ee4acd
rustam <iusupov.rustam1@gmail.com>
2022-03-14 16:44:58 +0100

    mon projet fil rouge

--- src/Controller/MyOrdersController.php
+++ src/Controller/MyOrdersController.php
@@ -17,15 +17,15 @@ class MyOrdersController extends AbstractController
     /**
      * @Route("/my_orders", name="my_orders")
      */
-    public function index(UserRepository $user, CategoryRepository $repocat, ClientRepository $repord, OrderRepository $ord): Response
+    public function index(UserRepository $user, CategoryRepository $repocat, OrderRepository $ord): Response
     {
         $idclient = $user->find($this->getUser())->getClient();
      //   dd($idclient);
         $orderClient = $ord->findBy(['client'=>$idclient]);
        // dd($orderClient);
         $categories = $repocat->findAll();
-        $client = $repord->find($idclient);
-        $list = $client->getOrders();
+//        $client = $repord->find($idclient);
+//        $list = $client->getOrders();
 dump($orderClient);
         return $this->render('my_orders/index.html.twig', [
             'categories' => $categories,
