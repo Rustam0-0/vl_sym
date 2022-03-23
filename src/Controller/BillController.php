4f3657b515ed86ef411560cbf5eb6ab8e21a2ac0
rustam <iusupov.rustam1@gmail.com>
2022-03-23 11:26:36 +0100

    mon projet fil rouge

---
+++ src/Controller/BillController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Order;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BillController extends AbstractController
+{
+    /**
+     * @Route("/bill/{order}", name="bill")
+     */
+    public function index(Order $order)
+    {
+        $pdfOptions = new Options();
+        $pdfOptions->set(array('defaultFont'=> 'Arial', 'isRemoteEnabled'=>true));
+        $dompdf = new Dompdf($pdfOptions);
+        $html = $this->renderView('bill/index.html.twig', [
+            'order' => $order,
+        ]);
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $context = stream_context_create([
+            'ssl' => [
+                'verify_peer' => FALSE,
+                'verify_peer_name' => FALSE
+            ]
+        ]);
+        $dompdf->setHttpContext($context);
+        $dompdf->render();
+        return $dompdf->stream("mypdf.pdf", [
+            "Attachment" => false
+        ]);
+    }
+}
